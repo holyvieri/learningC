ccb4ede8b72845a8d52fd33b410a78d9d9550d0c
Ana Oliveira <124950249+holyvieri@users.noreply.github.com>
2023-03-28 23:05:04 -0300

    task alura
    Multiplicar dois números

---
+++ task alura.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	int x; 
+	int y;
+	printf("tell me only two numbers pls: \n");
+	scanf("%d", &x);
+    scanf("%d", &y);
+		
+		int R = x * y ;
+
+	printf("now, we will multiply them...and here it is: %d x %d = %d\n", x, y, R);
+	
+	}
